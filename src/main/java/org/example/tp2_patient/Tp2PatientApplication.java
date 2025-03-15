bd303fccb055bbbff566b2bc3d5f01d6489c2aaa
rabihrajaa <rajaarabihh@gmail.com>
2025-03-15 14:57:00 +0000

    Initial commit

---
+++ src/main/java/org/example/tp2_patient/Tp2PatientApplication.java
@@ -0,0 +1,13 @@
+package org.example.tp2_patient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp2PatientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Tp2PatientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/tp2_patient/Tp2PatientApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.tp2_patient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp2PatientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
