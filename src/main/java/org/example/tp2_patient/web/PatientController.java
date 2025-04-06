b72acae4a90d3b24008c4d3d7aa52e6f36b067c3
rabihrajaa <rajaarabihh@gmail.com>
2025-04-06 16:27:09 +0100

    liste_patients_page

--- src/main/java/org/example/tp2_patient/web/PatientController.java
+++ src/main/java/org/example/tp2_patient/web/PatientController.java
@@ -1,9 +1,13 @@
 package org.example.tp2_patient.web;
 
 import lombok.AllArgsConstructor;
+import org.example.tp2_patient.entites.Patient;
 import org.example.tp2_patient.repository.PatientRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 @AllArgsConstructor
@@ -11,8 +15,9 @@ public class PatientController {
     private PatientRepository patientRepository;
 
     @GetMapping("/index")
-    public String index(){
-        System.out.println("page index");
+    public String index(Model model){
+        List<Patient> listpatients=patientRepository.findAll();
+        model.addAttribute("ListPatients",listpatients);
         return "patients";
     }
 
