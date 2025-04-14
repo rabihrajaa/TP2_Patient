1315ef1e3754284c9651d284ae91bf38e49173f7
rabihrajaa <rajaarabihh@gmail.com>
2025-04-14 21:10:47 +0100

    add2

--- src/main/java/org/example/tp2_patient/entites/Patient.java
+++ src/main/java/org/example/tp2_patient/entites/Patient.java
@@ -1,13 +1,11 @@
 package org.example.tp2_patient.entites;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.util.Date;
 
@@ -17,6 +15,8 @@ public class Patient {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nom;
+    @Temporal(TemporalType.DATE)
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date dateNaissane;
     private Boolean malade;
     private int score;
